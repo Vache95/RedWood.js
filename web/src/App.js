34bd9a8e34e2326c64b27c62e7fc73866e34cac9
Vache <vache.virabyan@codetime.am>
2022-12-24 15:33:18 +0400

    chakra

--- web/src/App.js
+++ web/src/App.js
@@ -2,14 +2,51 @@ import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
-import * as theme from 'config/chakra.config'
+// import * as theme from 'config/chakra.config'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './index.css'
 
-const extendedTheme = extendTheme(theme)
+const extendedTheme = extendTheme({
+  components: {
+    Button: {
+      // 1. We can update the base styles
+      baseStyle: {
+        fontWeight: 'bold', // Normally, it is "semibold"
+      },
+      // 2. We can add a new button size or extend existing
+      sizes: {
+        xl: {
+          h: '56px',
+          fontSize: 'lg',
+          px: '32px',
+        },
+      },
+      // 3. We can add a new visual variant
+      variants: {
+        'with-shadow': {
+          bg: 'red.400',
+          boxShadow: '0 0 2px 4px #efdfde',
+        },
+        // 4. We can override existing variants
+
+        // 5. We can add responsive variants
+        sm: {
+          bg: 'teal.500',
+          fontSize: 'md',
+        },
+      },
+      // 6. We can overwrite defaultProps
+      defaultProps: {
+        size: 'lg', // default is md
+        variant: 'sm', // default is solid
+        colorScheme: 'green', // default is gray
+      },
+    },
+  },
+})
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,21 +1,68 @@
-import { Link, routes } from '@redwoodjs/router'
+import {
+  AlertDialog,
+  AlertDialogContent,
+  Button,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  useDisclosure,
+  AlertDialogOverlay,
+} from '@chakra-ui/react'
 import { MetaTags } from '@redwoodjs/web'
 import { useEffect } from 'react'
 import './homepage.scss'
 
 const HomePage = () => {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+  }, [])
 
-useEffect(()=>{
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
-},[])
-
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       <main>
         <h1>home</h1>
+        <Button
+          colorScheme="red"
+          onClick={onOpen}
+          w="200px"
+          h="50px"
+          fontSize="lg"
+          variant="with-shadow"
+        >
+          Delete Customer
+        </Button>
+
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Delete Customer
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                Are you sure? You can't undo this action afterwards.
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button colorScheme="red" onClick={onClose} ml={3}>
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
       </main>
     </>
   )
